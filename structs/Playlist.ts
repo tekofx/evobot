e1e9c79d7bfd4923634ff1c664cfb177476e0663
tekofx <tekofxx@gmail.com>
2023-09-08 22:02:02 +0200

    Fixed lyrics command

--- structs/Playlist.ts
+++ structs/Playlist.ts
@@ -1,7 +1,7 @@
 import youtube, { Playlist as YoutubePlaylist } from "youtube-sr";
 import { config } from "../utils/config";
 import { Song } from "./Song";
-import SpotifyWebApi from 'spotify-web-api-node';
+import SpotifyWebApi from "spotify-web-api-node";
 
 const pattern = /^.*(youtu.be\/|list=)([^#\&\?]*).*/i;
 
@@ -17,6 +17,7 @@ export class Playlist {
       .slice(0, config.MAX_PLAYLIST_SIZE - 1)
       .map((video) => {
         return new Song({
+          query: video.title!,
           title: video.title!,
           url: `https://youtube.com/watch?v=${video.id}`,
           duration: video.duration / 1000
@@ -42,15 +43,15 @@ export class Playlist {
   public static async fromSpotify(spotifyUrl: string) {
     const spotifyApi = new SpotifyWebApi({
       clientId: config.SPOTIFY_CLIENT_ID,
-      clientSecret: config.SPOTIFY_CLIENT_SECRET,
+      clientSecret: config.SPOTIFY_CLIENT_SECRET
     });
 
     // Retrieve an access token
     const data = await spotifyApi.clientCredentialsGrant();
-    spotifyApi.setAccessToken(data.body['access_token']);
+    spotifyApi.setAccessToken(data.body["access_token"]);
 
     // Get the playlist ID from the URL
-    const playlistId = spotifyUrl.split('/playlist/')[1].split('?')[0];
+    const playlistId = spotifyUrl.split("/playlist/")[1].split("?")[0];
 
     // Get the Spotify playlist
     const playlistResponse = await spotifyApi.getPlaylist(playlistId);
@@ -59,8 +60,8 @@ export class Playlist {
     // Get the tracks in the Spotify playlist
     const tracksResponse = await spotifyApi.getPlaylistTracks(playlistId);
     const tracks = tracksResponse.body.items
-      .filter(item => item.track !== null)
-      .map(item => item.track!.name+" "+item.track!.artists[0].name);
+      .filter((item) => item.track !== null)
+      .map((item) => item.track!.name + " " + item.track!.artists[0].name);
 
     // Search for each track on YouTube and add it to a new YouTube playlist
     const youtubePlaylist = new YoutubePlaylist();
@@ -72,5 +73,4 @@ export class Playlist {
 
     return new this(youtubePlaylist);
   }
-
 }
